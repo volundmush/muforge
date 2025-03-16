23d2d9e8f83fbc6b6ab44e8d0ad8cae08da1ef8c
Andrew Bastien <volundmush@gmail.com>
2025-03-16 02:34:20 -0400

    Patching things up.

--- mudforge/portal/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import asyncio
-import mudforge
-from mudforge.utils import run_program, get_config
-
-
-if __name__ == "__main__":
-    settings = get_config("portal")
-    asyncio.run(run_program("portal", settings), debug=True)
